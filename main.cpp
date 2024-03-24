a367d1cf74957a8e6b671ad235b5ac8c2dd71986
leoprosy <leprosy684@gmail.com>
2024-03-24 20:44:56 +0100

    generate the board with 2 random tiles initialized

--- main.cpp
+++ main.cpp
@@ -1,57 +1,50 @@
 #include <iostream>
 #include <fstream>      
 #include <vector>
-#include <tuple>
+#include <random>
 #include "print.cpp"
 using namespace std;
 
-//int board[4][4] = { 0 };
-vector<vector<int>> board(4, vector<int>(4));
+int board[4][4] = { 0 };
 
 static int random(int offset, int range) {
-	srand((unsigned)time(NULL));
-	int random = offset + (rand() % range);
-	return random;
+	random_device rd;
+	uniform_int_distribution<int> dist(offset, range);
+	return dist(rd);
 };
 
-static void print_matrix(vector<vector<int>> v) {
-	for (int i = 0; i < size(v); i++) {
-		for (int j = 0; j < size(v[i]); j++) {
-			cout << v[i][j] << " ";
+template<size_t rows, size_t cols>
+vector<pair<int, int>> get_empty(int(&board)[rows][cols]){
+	vector<pair<int, int>> pos;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (board[i][j] == 0) {
+				pos.push_back(make_pair(i, j));
+			}
 		}
-		cout << endl;
 	}
-}
-
-vector<tuple<int, int>> get_empty() {
-	vector<tuple<int, int>> pos = {};
-	for (int i = 0; i < size(board); ++i) for (int j = 0; j < size(board[i]); ++j) if (board[i][j] == 0) pos.emplace_back(i, j);
-
-	/*for (tuple<int, int> i : pos) { 
-		print_tuple(i);
-	}*/
 
 	return pos;
 };
 
-void add_cell() {
-	int next = random(1, 17);
-	next = (next == 16) ? 4 : 2;
-	tuple<int, int> news = get_empty()[random(0,16)];
+static void add_cell() {
+	int next = random(0,17);
+	next = (next == 1) ? 4 : 2;
+	pair<int, int> news = get_empty(board)[random(0,17)];
+	cout << news.first << news.second << endl;
 	board[get<0>(news)][get<1>(news)] = next;
+
 	return;
 };
 
 int main() {
 
-	/*for (int i = 0; i < 2; i++) {
+	for (int i = 0; i < 2; i++) {
 		add_cell();
-	};
+	}
 
-	print_matrix(board);*/
-
-	int a[2] = { 0,1 };
-	print_array(a);
+	print_matrix(board);
 
 	return 0;
 };
--- print.cpp
+++ print.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <tuple>
+#include <vector>
 
 using namespace std;
 
 template<size_t N>
-int print_array(int (&t)[N]) {
+void print_array(int (&t)[N]) {
 	int size = N;
 	for (int i = 0; i < size; i++) {
 		if (i == size - 1) {
@@ -15,5 +16,14 @@ int print_array(int (&t)[N]) {
 		}
 	}
 	cout << endl;
-	return size;
 };
+
+template<size_t rows, size_t cols>
+void print_matrix(int (&t)[rows][cols]) {
+	for (int (&i)[cols] : t) {
+		for (int j : i) {
+			cout << j << " ";
+		}
+		cout << endl;
+	}
+}
